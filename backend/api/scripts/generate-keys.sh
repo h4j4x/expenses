d05decd1fb197a586ac099aad5fa6dfcc06c8eee
h4j4x <miche.atucha@gmail.com>
2022-12-28 10:52:38 -0500

    avoid storing keys

--- backend/api/scripts/generate-keys.sh
+++ backend/api/scripts/generate-keys.sh
@@ -3,4 +3,5 @@ openssl genrsa -out rsaPrivateKey.pem 2048
 openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
 openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
 
+mkdir -p ./src/main/resources/keys
 mv ./*.pem ./src/main/resources/keys

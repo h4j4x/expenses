804446380eb0fd2b169d62598ab1843e2e7403ee
h4j4x <miche.atucha@gmail.com>
2022-12-28 15:03:03 -0500

    user resource sign in

--- backend/api/src/main/java/com/h4j4x/expenses/api/resource/UserResource.java
+++ backend/api/src/main/java/com/h4j4x/expenses/api/resource/UserResource.java
@@ -1,6 +1,8 @@
 package com.h4j4x.expenses.api.resource;
 
+import com.h4j4x.expenses.api.model.Auth;
 import io.smallrye.jwt.build.Jwt;
+import io.smallrye.mutiny.Uni;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
@@ -12,23 +14,27 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/users")
 public class UserResource {
+    static final String USER_ROLE = "user";
+
     @ConfigProperty(name = "mp.jwt.verify.issuer")
     String jwtIssuer;
 
     @POST
     @PermitAll
     @Path("/sign-in")
-    public String signIn() { // todo: payload
-        return Jwt.issuer(jwtIssuer)
-            .upn("jdoe@quarkus.io") // todo: from user
-            .groups("user")
+    public Uni<String> signIn(Auth auth) {
+        String token = Jwt.issuer(jwtIssuer)
+            .upn(auth.getEmail())
+            .groups(USER_ROLE)
             .sign();
+        return Uni.createFrom().item(token);
     }
 
     @GET
-    @RolesAllowed("user")
+    @RolesAllowed(USER_ROLE)
     @Path("/me")
-    public String me(@Context SecurityContext securityContext) {
-        return securityContext.getUserPrincipal().getName();
+    public Uni<String> me(@Context SecurityContext securityContext) {
+        String name = securityContext.getUserPrincipal().getName();
+        return Uni.createFrom().item(name);
     }
 }
--- backend/api/src/test/java/com/h4j4x/expenses/api/resource/UserResourceTests.java
+++ backend/api/src/test/java/com/h4j4x/expenses/api/resource/UserResourceTests.java
@@ -1,18 +1,21 @@
 package com.h4j4x.expenses.api.resource;
 
+import com.h4j4x.expenses.api.model.Auth;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
 @TestHTTPEndpoint(UserResource.class)
 public class UserResourceTests {
+    static final String TEST_EMAIL = "test@mail.com";
+
     @Test
     public void whenGetMe_Anonymous_Then_Throw401() {
-        given()
+        RestAssured.given()
             .when().get("me")
             .then()
             .statusCode(401);
@@ -20,29 +23,31 @@ public class UserResourceTests {
 
     @Test
     public void whenPostSignIn_Then_ReturnJwtToken() {
-        given()
-            .when().post("sign-in")
+        Auth auth = new Auth(TEST_EMAIL, "");
+        RestAssured.given()
+            .headers("Content-Type", "application/json")
+            .when().body(auth).post("sign-in")
             .then()
             .statusCode(200)
             .body(not(blankOrNullString()));
     }
 
     @Test
-    @TestSecurity(user = "test@mail.com", roles = "other")
+    @TestSecurity(user = TEST_EMAIL, roles = "other")
     public void whenGetMe_AuthenticatedWithoutUserRole_Then_Throw403() {
-        given()
+        RestAssured.given()
             .when().get("me")
             .then()
             .statusCode(403);
     }
 
     @Test
-    @TestSecurity(user = "test@mail.com", roles = "user")
+    @TestSecurity(user = TEST_EMAIL, roles = UserResource.USER_ROLE)
     public void whenGetMe_AuthenticatedWithUserRole_Then_ReturnUserData() {
-        given()
+        RestAssured.given()
             .when().get("me")
             .then()
             .statusCode(200)
-            .body(is("test@mail.com"));
+            .body(is(TEST_EMAIL));
     }
 }

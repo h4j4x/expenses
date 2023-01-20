6be33bcbb65cfb989b8bd961ca9591e5ddb18d90
h4j4x <miche.atucha@gmail.com>
2023-01-20 15:29:59 -0500

    jacoco

--- backend/common/src/test/java/com/h4j4x/expenses/common/util/NumberUtilsTests.java
+++ backend/common/src/test/java/com/h4j4x/expenses/common/util/NumberUtilsTests.java
@@ -5,26 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class NumberUtilsTests {
     private final Faker faker = new Faker(new Locale("en-US"));
 
     @TestFactory
-    List<DynamicTest> testParseLong() {
+    List<DynamicTest> whenParseValidLong_Then_ShouldReturnValue() {
         List<DynamicTest> tests = new ArrayList<>();
         for (int i = 10; i <= 100; i += 10) {
-            tests.add(testParseLong(faker.random().nextLong()));
+            tests.add(parseValidLong(faker.random().nextLong()));
         }
         return tests;
     }
 
-    private DynamicTest testParseLong(long value) {
+    private DynamicTest parseValidLong(long value) {
         return DynamicTest.dynamicTest("Test NumberUtils.parseLong",
             () -> {
                 var parsed = NumberUtils.parseLong(String.valueOf(value));
                 assertEquals(value, parsed);
             });
+    }
+
+    @Test
+    public void whenParseInvalidLong_Then_ShouldReturnNull() {
+        assertNull(NumberUtils.parseLong("-"));
+    }
+
+    @Test
+    public void whenParseNull_Then_ShouldReturnNull() {
+        assertNull(NumberUtils.parseLong(null));
     }
 }
